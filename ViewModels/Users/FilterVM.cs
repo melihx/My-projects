a157644ac61c4558e92d546934b0e46914080929
melihx <120415948+melihx@users.noreply.github.com>
2023-02-14 00:36:53 +0200

    Add files via upload

---
+++ ViewModels/Users/FilterVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadABook.ViewModels.Users
+{
+    public class FilterVM
+    {
+        [DisplayName("Username: ")]
+        public string Username { get; set; }
+
+        [DisplayName("First Name: ")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name: ")]
+        public string LastName { get; set; }
+    }
+}
---
+++ ViewModels/Users/IndexVM.cs
@@ -0,0 +1,16 @@
+﻿using ReadABook.Entities;
+using ReadABook.ViewModels.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadABook.ViewModels.Users
+{
+    public class IndexVM
+    {
+        public List<User> Items { get; set; }
+        public PagerVM Pager { get; internal set; }
+        public FilterVM Filter { get; internal set; }
+    }
+}
